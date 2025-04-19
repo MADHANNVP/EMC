a78b515a4a70293cf72901f9481b02687763735c
MADHANNVP <blackrose21082006@gmail.com>
2025-04-19 17:18:43 +0530

    update studentfile

--- StudentForm/studentform.js
+++ StudentForm/studentform.js
@@ -2,6 +2,10 @@ var navscr = document.querySelector(".navbar--scroll")
 var btn1 = document.querySelector(".navbar--btn")
 var body = document.querySelector(".container")
 var body1 = document.querySelector(".navbar--heading")
+var navbar1=document.querySelector(".navbar--2--scroll")
+var navbar2=document.querySelector(".student--information")
+var std=document.querySelector(".navbar--scroll")
+var stdin=document.querySelector(".student--information")
 btn1.addEventListener("click", function () {
     navscr.classList.add("show")
 })
@@ -10,4 +14,20 @@ body.addEventListener("click", function () {
 })
 body1.addEventListener("click", function () {
     navscr.classList.remove("show")
+})
+std.addEventListener("click",function(){
+     stdin.classList.add("hidden")
+})
+var navscr1=document.querySelector(".navbar--2--scroll")
+var btn2=document.querySelector(".navbar--2--btn")
+// var body2=document.querySelector(".")
+var body3=document.querySelector(".navbar--2--heading")
+btn2.addEventListener("click",function(){
+    navscr1.classList.add("view")
+})
+body3.addEventListener("click", function () {
+    navscr1.classList.remove("view")
+})
+navbar1.addEventListener("click",function(){
+    navbar2.classList.remove("hidden")
 })
