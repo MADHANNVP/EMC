57dc91f881e1e586d332170b847db32ea8a34d57
MADHANNVP <blackrose21082006@gmail.com>
2025-04-19 21:13:27 +0530

    update studentform

--- StudentForm/studentform.js
+++ StudentForm/studentform.js
@@ -34,3 +34,37 @@ navbar1.addEventListener("click",function(){
 navbar1.addEventListener("click",function(){
     navbartwo.classList.add("hidden")
 })
+navscr.addEventListener("click",function(){
+    navbartwo.classList.remove("hidden")
+})
+function resetForm() {
+    document.getElementById("myForm").reset(); // clears all input fields
+}
+var first_name=document.querySelector(".first--name")
+var last_name=document.querySelector(".last--name")
+var date=document.querySelector(".date")
+var save__btn=document.querySelector(".save--div")
+var student__table=document.querySelector(".student--table")
+save__btn.addEventListener("click",function(){
+    console.log(first_name.value)
+var tr1=document.createElement("tr")
+var td1=document.createElement("td")
+var td2=document.createElement("td")
+var td3=document.createElement("td")
+var td4=document.createElement("td")
+var td5=document.createElement("td")
+var td6=document.createElement("td")
+var td7=document.createElement("td")
+var td8=document.createElement("td")
+var td9=document.createElement("td")
+var td10=document.createElement("td")
+var td11=document.createElement("td")
+var td12=document.createElement("td")
+td1.textContent=first_name.value
+td2.textContent=last_name.value
+td3.textContent=date.value
+tr1.appendChild(td1)
+tr1.appendChild(td2)
+tr1.appendChild(td3)
+student__table.append(tr1)
+})
